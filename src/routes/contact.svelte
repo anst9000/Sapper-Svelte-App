99f1abb2488ae7bd7e33dcd1ad3657e5384070ae
anst9000 <coltlaboy@yahoo.se>
2020-09-04 23:26:12 +0200

    first commit

---
+++ src/routes/contact.svelte
@@ -0,0 +1,27 @@
+<script>
+	console.log('We are on contact page');
+</script>
+
+
+<svelte:head>
+	<title>Contact</title>
+</svelte:head>
+
+<h1>Contact Job Ninja</h1>
+
+<p>
+Lorem ipsum dolor sit amet consectetur adipisicing elit.
+Fuga beatae adipisci mollitia inventore saepe praesentium ipsam?
+Pariatur excepturi quia, nesciunt placeat ullam temporibus fuga ab enim non,
+id veniam quibusdam ratione. Modi quos magnam quaerat quam exercitationem odit
+enim distinctio velit quae libero repudiandae vero repellat mollitia, est aliquam.
+Doloremque!
+</p>
+<p>
+Lorem ipsum dolor sit amet consectetur adipisicing elit.
+Fuga beatae adipisci mollitia inventore saepe praesentium ipsam?
+Pariatur excepturi quia, nesciunt placeat ullam temporibus fuga ab enim non,
+id veniam quibusdam ratione. Modi quos magnam quaerat quam exercitationem odit
+enim distinctio velit quae libero repudiandae vero repellat mollitia, est aliquam.
+Doloremque!
+</p>
